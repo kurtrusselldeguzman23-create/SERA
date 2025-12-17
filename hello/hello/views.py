414c074312539f80789783d3d20a26fcc4480761
Kuruuii <kurtrusselldeguzman23@gmail.com>
2025-12-17 20:59:28 +0800

    first commit

---
+++ hello/hello/views.py
@@ -0,0 +1,14 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def homepage(request):
+    #return HttpResponse("niggas in paris")
+    return render(request, 'home.html')
+    
+
+
+def about(request):
+    #return HttpResponse("my about page")
+    return render(request, 'about.html')
---
+++ hello/static/js/main.js
@@ -0,0 +1,1 @@
+console.log(' this is ur js from your about page');
