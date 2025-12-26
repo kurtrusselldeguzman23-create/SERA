aad5a02a893e3ab146150c290dd60525822a828b
Kuruuii <kurtrusselldeguzman23@gmail.com>
2025-12-26 20:22:31 +0800

    changes

--- hello/posts/views.py
+++ hello/posts/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    posts=Post.objects.all()
+    posts=Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
